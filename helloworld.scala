f142d58dcf36306c57175acf208e188fbe68c9ce
Vyas <41269385+iamvyas@users.noreply.github.com>
2023-03-24 21:57:26 +0530

    scala is here
    inime ella mass dha

---
+++ helloworld.scala
@@ -0,0 +1,7 @@
+object HelloWorld{
+  def main(args: Array[String]) :Unit= {
+    println("Hello world")
+    println("escape sequences ,\n \"\\n\" goes to new line , reserved char used inside string needs prefix with back slash")
+    println("this  \t goes some spaces away")
+  }
+}
